3f3fcc3668954a939d77ec322f9ed06439111be6
Darwin Gomez <gmzdwn6@gmail.com>
2024-11-30 16:53:21 -0500

    xd

--- taller recuperacion/ej_7/index.js
+++
@@ -1,8 +0,0 @@
-function espalindromo(palabra) {
-    const palabras = palabra.toLowerCase().replace(/\s+/g, '');
-    const palabrass = palabras.split('').reverse().join('');
-    return palabras === palabrass;
-}
-console.log(espalindromo("ana"))
-console.log(espalindromo("hola"))  
-console.log(espalindromo("Anita lava la tina"))
